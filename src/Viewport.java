4e47bead480ad54db0bbc8f5f10a3f5fd1b59525
Danny Dang <91161014+dannydang-cp@users.noreply.github.com>
2023-01-26 18:34:11 -0800

    Add files via upload

---
+++ src/Viewport.java
@@ -0,0 +1,42 @@
+import java.util.Random;
+
+public final class Viewport {
+    public int row;
+    public int col;
+    public int numRows;
+    public int numCols;
+
+    public Viewport(int numRows, int numCols) {
+        this.numRows = numRows;
+        this.numCols = numCols;
+    }
+
+    public static int getIntFromRange(int max, int min) {
+        Random rand = new Random();
+        return min + rand.nextInt(max-min);
+    }
+
+    public static double getNumFromRange(double max, double min) {
+        Random rand = new Random();
+        return min + rand.nextDouble() * (max - min);
+    }
+
+    public Point viewportToWorld(int col, int row) {
+        return new Point(col + this.col, row + this.row);
+    }
+
+    public Point worldToViewport(int col, int row) {
+        return new Point(col - this.col, row - this.row);
+    }
+
+    public void shift(int col, int row) {
+        this.col = col;
+        this.row = row;
+    }
+
+    public boolean contains(Point p) {
+        return p.y >= this.row && p.y < this.row + this.numRows && p.x >= this.col && p.x < this.col + this.numCols;
+    }
+
+
+}
---
+++ src/WorldModel.java
@@ -0,0 +1,165 @@
+import processing.core.PImage;
+
+import java.util.*;
+
+/**
+ * Represents the 2D World in which this simulation is running.
+ * Keeps track of the size of the world, the background image for each
+ * location in the world, and the entities that populate the world.
+ */
+public final class WorldModel {
+    public int numRows;
+    public int numCols;
+    public Background[][] background;
+    public Entity[][] occupancy;
+    public Set<Entity> entities;
+
+    public WorldModel() {
+
+    }
+
+    public void tryAddEntity(Entity entity) {
+        if (isOccupied(entity.position)) {
+            // arguably the wrong type of exception, but we are not
+            // defining our own exceptions yet
+            throw new IllegalArgumentException("position occupied");
+        }
+
+        addEntity(entity);
+    }
+
+    public boolean withinBounds(Point pos) {
+        return pos.y >= 0 && pos.y < this.numRows && pos.x >= 0 && pos.x < this.numCols;
+    }
+
+    public boolean isOccupied(Point pos) {
+        return withinBounds(pos) && getOccupancyCell(pos) != null;
+    }
+
+    public Optional<Entity> nearestEntity(List<Entity> entities, Point pos) {
+        if (entities.isEmpty()) {
+            return Optional.empty();
+        } else {
+            Entity nearest = entities.get(0);
+            int nearestDistance = pos.distanceSquared(nearest.position);
+
+            for (Entity other : entities) {
+                int otherDistance = pos.distanceSquared(other.position);
+
+                if (otherDistance < nearestDistance) {
+                    nearest = other;
+                    nearestDistance = otherDistance;
+                }
+            }
+
+            return Optional.of(nearest);
+        }
+    }
+
+    public Optional<Entity> findNearest(Point pos, List<EntityKind> kinds) {
+        List<Entity> ofType = new LinkedList<>();
+        for (EntityKind kind : kinds) {
+            for (Entity entity : this.entities) {
+                if (entity.kind == kind) {
+                    ofType.add(entity);
+                }
+            }
+        }
+
+        return nearestEntity(ofType, pos);
+    }
+
+    /*
+       Assumes that there is no entity currently occupying the
+       intended destination cell.
+    */
+    public void addEntity(Entity entity) {
+        if (withinBounds(entity.position)) {
+            setOccupancyCell(entity.position, entity);
+            this.entities.add(entity);
+        }
+    }
+
+    public void moveEntity(EventScheduler scheduler, Entity entity, Point pos) {
+        Point oldPos = entity.position;
+        if (withinBounds(pos) && !pos.equals(oldPos)) {
+            setOccupancyCell(oldPos, null);
+            Optional<Entity> occupant = getOccupant(pos);
+            occupant.ifPresent(target -> removeEntity(scheduler, target));
+            setOccupancyCell(pos, entity);
+            entity.position = pos;
+        }
+    }
+
+    public void removeEntity(EventScheduler scheduler, Entity entity) {
+        scheduler.unscheduleAllEvents(entity);
+        removeEntityAt(entity.position);
+    }
+
+    public void removeEntityAt(Point pos) {
+        if (withinBounds(pos) && getOccupancyCell(pos) != null) {
+            Entity entity = getOccupancyCell(pos);
+
+            /* This moves the entity just outside of the grid for
+             * debugging purposes. */
+            entity.position = new Point(-1, -1);
+            this.entities.remove(entity);
+            setOccupancyCell(pos, null);
+        }
+    }
+
+
+    public Optional<Entity> getOccupant(Point pos) {
+        if (isOccupied(pos)) {
+            return Optional.of(getOccupancyCell(pos));
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    public Entity getOccupancyCell(Point pos) {
+        return this.occupancy[pos.y][pos.x];
+    }
+
+    public void setOccupancyCell(Point pos, Entity entity) {
+        this.occupancy[pos.y][pos.x] = entity;
+    }
+
+    public Background getBackgroundCell(Point pos) {
+        return this.background[pos.y][pos.x];
+    }
+
+    public void setBackgroundCell(Point pos, Background background) {
+        this.background[pos.y][pos.x] = background;
+    }
+
+    public Optional<PImage> getBackgroundImage(Point pos) {
+        if (withinBounds(pos)) {
+            return Optional.of(this.getCurrentImage(getBackgroundCell(pos)));
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    public PImage getCurrentImage(Object object) {
+        if (object instanceof Background background) {
+            return background.images.get(background.imageIndex);
+        } else if (object instanceof Entity entity) {
+            return entity.images.get(entity.imageIndex % entity.images.size());
+        } else {
+            throw new UnsupportedOperationException(String.format("getCurrentImage not supported for %s", object));
+        }
+    }
+
+    /**
+     * Helper method for testing. Don't move or modify this method.
+     */
+    public List<String> log(){
+        List<String> list = new ArrayList<>();
+        for (Entity entity : entities) {
+            String log = entity.log();
+            if(log != null) list.add(log);
+        }
+        return list;
+    }
+}
